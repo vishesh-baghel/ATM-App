1916dab421817ac10078c3284c6b12415774014a
Vishesh Baghel <71141511+vishesh-baghel@users.noreply.github.com>
2022-03-28 10:35:07 +0530

    Learned a lot about scope chaining and variable lookup

--- script-home.js
+++ script-home.js
@@ -21,3 +21,28 @@
 // a scope will never, ever have access to the variables of an inner scope
 
 // This is just for the commits
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}You are ${age}, born in ${birthYear}`;
+    alert(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      var millenial = true;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+    add(2, 3);
+  }
+  return age;
+}
+
+const firstName = "vishesh";
+calcAge(1999);
