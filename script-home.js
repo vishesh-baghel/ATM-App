51079e1a8dac7b82f04df1b3498265b09d806ae5
Vishesh Baghel <71141511+vishesh-baghel@users.noreply.github.com>
2022-04-05 08:15:00 +0530

    Implemented the lazy loading feature

--- script-home.js
+++ script-home.js
@@ -294,3 +294,30 @@ allSections.forEach(function(section) {
   section.classList.add('section--hidden'
   );
 });
+
+// Lazy loading images 
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function() {
+    entry.target.classList.remove('lazy-img');
+  })
+  observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null, 
+  threshold: 0,
+  rootMargin: '-200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
